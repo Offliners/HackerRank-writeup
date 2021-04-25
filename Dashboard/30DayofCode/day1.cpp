d734c49f8019046ee47d3ad8ad4ff8f7deb8529d
Offliner <42777325+Offliners@users.noreply.github.com>
2021-04-25 10:25:49 +0800

    Update day1.cpp

--- Dashboard/30DayofCode/day1.cpp
+++ Dashboard/30DayofCode/day1.cpp
@@ -14,18 +14,23 @@ int main() {
     int j;
     double e;
     string m;
+    
     // Read and save an integer, double, and String to your variables.
     // Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
     cin >> j;
     cin >> e;
     cin.ignore();
     getline(cin, m);
+    
     // Print the sum of both integer variables on a new line.
     cout << i + j << endl;
+    
     // Print the sum of the double variables on a new line.
     cout << fixed << setprecision(1) << d + e << endl; 
+    
     // Concatenate and print the String variables on a new line
     // The 's' variable above should be printed first.
     cout << s + m << endl;
+    
     return 0;
 }
